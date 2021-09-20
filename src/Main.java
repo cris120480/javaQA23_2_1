db421e2164d8b81fb1060c238b0806eb3ea66d43
Денис Гришкевич <cris12@mail.ru>
2021-09-20 20:28:57 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean registered = true;
+        float ticketPrice = 1200.70F;
+        float bonusGrade = 20F;
+        float bonusBalance = (int)ticketPrice / (int) bonusGrade;
+
+           System.out.println(bonusBalance);
+    }
+    }
